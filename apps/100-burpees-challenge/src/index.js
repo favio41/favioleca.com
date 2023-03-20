5ae82ee6ff36f3ee11fa8de4debbf17f5e40af8b
favio41 <favio41@gmail.com>
2023-03-20 12:08:50 +0000

    Remove 100-burpees-challenge-app

--- apps/100-burpees-challenge/src/index.js
+++
@@ -1,9 +0,0 @@
-const entryLogs = require("./pages/entry-logs");
-const entryLogCreate = require("./pages/entry-log-create");
-const dashboard = require("./pages/dashboard");
-
-document.addEventListener("alpine:init", () => {
-  Alpine.data("entryLogs", entryLogs);
-  Alpine.data("entryLogCreate", entryLogCreate);
-  Alpine.data("dashboard", dashboard);
-});
